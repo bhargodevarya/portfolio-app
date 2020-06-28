f8d99917a1e3b148c3a670c79e6cf0e639a1aeba
Bhargodev Arya <bhargodevarya@gmail.com>
2020-06-28 17:36:25 +0530

    Adding new logo, removing typed and fontawesome dependency

--- component/RBNavBar.js
+++ component/RBNavBar.js
@@ -5,7 +5,7 @@ import React from "react";
 const RBNavBar = () => {
   return (
     <Navbar bg="light" expand="lg" id="myNavbar">
-      <Navbar.Brand href="#home"><img src="/logo_lpb_small.png"></img></Navbar.Brand>
+      <Navbar.Brand href="/"><img src="/logo_lpb_black_small.png"></img></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto" id="myNavItem">
