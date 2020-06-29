3d435cfe425d38caeefaf90d9adf7e849a72c0b1
Bhargodev <bhargodevarya@gmail.com>
2020-06-29 16:28:14 +0530

    Basic contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,68 +1,92 @@
-import React, { Component } from 'react'
-import Button from 'react-bootstrap/Button'
-import getMovies from '../data/data'
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import getMovies from "../data/data";
+
+import Form from "react-bootstrap/Form";
 
 class Contact extends Component {
+  static async getInitialProps() {
+    console.log("Hello");
+    const movies = await getMovies();
+    movies.map((m) => console.log(m.name));
+    return { movies };
+  }
 
-    static async getInitialProps() {
-        console.log("Hello")
-        const movies = await getMovies()
-        movies.map(m => console.log(m.name))
-        return {movies}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.decrement = this.decrement.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {count:0}
-        this.decrement = this.decrement.bind(this)
-    }
-
-    /*
+  /*
     You can initiate the state like this as well, but you wont be able to use is in any advanced use cases
     state = {
          count: 0
      }     
      */
 
-    //this.decrement = this.decrement.bind(this)
+  //this.decrement = this.decrement.bind(this)
 
-    increment = (e) => {
-        //arrow functions inherit the this context from the context where they are defined
-        //and therefore can be called directly from the component
-        e.preventDefault()
+  increment = (e) => {
+    //arrow functions inherit the this context from the context where they are defined
+    //and therefore can be called directly from the component
+    e.preventDefault();
 
-        let currentCount = this.state.count
+    let currentCount = this.state.count;
 
-        //This below statement does not set the state and hence does not lead to re-render
-        //this.state.count = currentCount +1
+    //This below statement does not set the state and hence does not lead to re-render
+    //this.state.count = currentCount +1
 
-        //setState must be called to change the state and lead to a re-render
-        this.setState({count: currentCount+1})
-    }
+    //setState must be called to change the state and lead to a re-render
+    this.setState({ count: currentCount + 1 });
+  };
 
-    /**
-     * Normal function dont the the this context. Therefore must either be binded in the constructor
-     * or be called via callback function from the actual component
-     */
-    decrement() {
-        this.setState({count: this.state.count-1})
-    }
-    
-    render() {
-        console.log(this.props)
-        return (
-            <div>
-                This is the contact us page<br></br>
-                <Button onClick={this.increment}>Increment</Button><br></br>
-                {/* 
+  /**
+   * Normal function dont the the this context. Therefore must either be binded in the constructor
+   * or be called via callback function from the actual component
+   */
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label style={{ color: "red" }}>Name</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="How should I address you?"
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicSubject">
+            <Form.Label style={{ color: "red" }}>Subject</Form.Label>
+            <Form.Control type="input" placeholder="What is this about?" />
+          </Form.Group>
+          <Form.Group controlId="formBasicText">
+            <Form.Label style={{ color: "red" }}>Thoughts</Form.Label>
+            <Form.Control type="text" placeholder="Ok, tell me" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        This is the contact us page<br></br>
+        <Button onClick={this.increment}>Increment</Button>
+        <br></br>
+        {/* 
                 if you dont want to bind in the constructor
                 <Button onClick={() => this.decrement()}>Decrement</Button><br></br> 
                 */}
-                <Button onClick={this.decrement}>Decrement</Button><br></br>
-                {this.state.count}
-            </div>
-        )
-    }
+        <Button onClick={this.decrement}>Decrement</Button>
+        <br></br>
+        {this.state.count}
+      </div>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
