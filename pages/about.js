6c202c2cac96593554242a93b88bc539f8fca9e0
Bhargodev Arya <bhargodevarya@gmail.com>
2020-06-30 17:42:31 +0530

    Updating about page with github card

--- pages/about.js
+++ pages/about.js
@@ -1,43 +1,58 @@
-import { useState } from 'react'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const About = () => {
-    /**
-     * useState is a hook which allows functional components to play with state
-     * Call it with intial state and it gives back an array of 2 elements
-     * first, the state and second the function to set it.
-     * 
-     * Use array destructuring to access them like below
-     */
-    const [count, setCount] = useState(0)
-    
+  /**
+   * useState is a hook which allows functional components to play with state
+   * Call it with intial state and it gives back an array of 2 elements
+   * first, the state and second the function to set it.
+   *
+   * Use array destructuring to access them like below
+   */
+  const [count, setCount] = useState(0);
 
-    /**
-     * Cant have normal functions here as it is a functional component.
-     * Only arrow functions work here.
-     */
-    const increment = () => {
-        setCount(count+1)
-    }
+  /**
+   * Cant have normal functions here as it is a functional component.
+   * Only arrow functions work here.
+   */
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-    const decrement = () => {
-        setCount(count-1)
-    }
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
-    return (
-        <Container>
-            <Row>
-                <Col style={{color:"red"}}>Github</Col>
-                <Col><a style={{color:"red"}} href="https://github.com/bhargodevarya">profile</a></Col>
-            </Row>
-            <Row>
-                <Col><img src="/github_profile_200px.svg"></img></Col>
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Row>
+        <Col style={{ color: "red" }}>Github</Col>
+        <Col>
+          <a style={{ color: "red" }} href="https://github.com/bhargodevarya">
+            profile
+          </a>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/github_profile_50px.svg" />
+            <Card.Body>
+              <Card.Title>Github Profile</Card.Title>
+              <Card.Text>
+                This is my github profile, feel free to visit.
+              </Card.Text>
+              <Button variant="link" href="https://github.com/bhargodevarya">Let's Github</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default About
+export default About;
