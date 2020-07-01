938967a78fc15816d29b669a4beeae7af218ab69
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-01 17:49:48 +0530

    Books page card for displaying books, initial commit

--- pages/book.js
+++ pages/book.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 class Book extends Component {
-    render() {
-        return (
-            <div style={{color:"red"}}>
-                This is the page to display my Book Recommendations
+  render() {
+    return (
+          <div id="bookId" className="card" style={{width: "18rem"}}>
+            <img className="card-img-top" src="..." alt="book Image" />
+            <div className="card-body">
+              <p className="card-text">
+                Text for the book
+              </p>
             </div>
-        )
-    }
+          </div>
+    );
+  }
 }
 
-export default Book
+export default Book;
