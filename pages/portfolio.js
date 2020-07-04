86ac2420347fa27c9108942813be5d0f8a4c06e3
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-04 17:34:53 +0530

    Added portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class portfolio extends Component {
+    render() {
+        return (
+            <div style={{color:"red"}}>
+                This is the portfolio page
+            </div>
+        )
+    }
+}
