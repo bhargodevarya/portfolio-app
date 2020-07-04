90d09f6dccd851673ec9867a90852e89f64babf4
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-04 19:36:17 +0530

    Fetching portfolio dummy data from portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import {getPortfolios} from '../data/data'
 
 export default class portfolio extends Component {
     render() {
+        console.log(this.props.portfolio[0].designation)
         return (
             <div style={{color:"red"}}>
                 This is the portfolio page
             </div>
         )
     }
+}
+
+export async function getStaticProps() {
+    //const response = await axios.get('http://jsonplaceholder.typicode.com/posts');
+    //console.log(response)
+    const portfolios = await getPortfolios()
+    return {props: portfolios}
 }
