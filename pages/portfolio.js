d64c93e74efd5758b0b2565ceb33854b65704d23
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-04 20:16:49 +0530

    Adding modal for portolio details

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,32 +3,59 @@ import axios from "axios";
 import { getPortfolios } from "../data/data";
 
 import Card from "react-bootstrap/Card";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button'
 
 export default class portfolio extends Component {
-  
-    processPortfolios(portfolios) {
+
+  constructor(props) {
+    super(props);
+    this.state= {
+        show: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({show: !this.state.show})
+    console.log(this.state.show);
+  }
+
+  processPortfolios(portfolios) {
     return portfolios.map((portfolio) => (
       <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>
-            {portfolio.designation} at{" "}
-            {portfolio.organization}
+            {portfolio.designation} at {portfolio.organization}
           </Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             {portfolio.location}
           </Card.Subtitle>
           <Card.Text>{portfolio.shortDesc}</Card.Text>
-          <Card.Link href="#">Details</Card.Link>
+          <Card.Link href="#" onClick={this.handleClick}>
+            Details
+          </Card.Link>
         </Card.Body>
       </Card>
     ));
   }
 
   render() {
-    console.log(this.props.portfolio[0].designation);    
+    console.log(this.props.portfolio[0].designation);
     return (
       <div>
-          {this.processPortfolios(this.props.portfolio)}
+        <Modal show={this.state.show} onHide={this.handleClick}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClick}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {this.processPortfolios(this.props.portfolio)}
       </div>
     );
   }
