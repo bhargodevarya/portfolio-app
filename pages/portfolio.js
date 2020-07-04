3801f99df7c547cb43780d921fca0c3d7d3df0d8
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-04 19:54:18 +0530

    Minor edits to dummy data and creating card for each portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import {getPortfolios} from '../data/data'
+import React, { Component } from "react";
+import axios from "axios";
+import { getPortfolios } from "../data/data";
+
+import Card from "react-bootstrap/Card";
 
 export default class portfolio extends Component {
-    render() {
-        console.log(this.props.portfolio[0].designation)
-        return (
-            <div style={{color:"red"}}>
-                This is the portfolio page
-            </div>
-        )
-    }
+  
+    processPortfolios(portfolios) {
+    return portfolios.map((portfolio) => (
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>
+            {portfolio.designation} at{" "}
+            {portfolio.organization}
+          </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {portfolio.location}
+          </Card.Subtitle>
+          <Card.Text>{portfolio.shortDesc}</Card.Text>
+          <Card.Link href="#">Details</Card.Link>
+        </Card.Body>
+      </Card>
+    ));
+  }
+
+  render() {
+    console.log(this.props.portfolio[0].designation);    
+    return (
+      <div>
+          {this.processPortfolios(this.props.portfolio)}
+      </div>
+    );
+  }
 }
 
 export async function getStaticProps() {
-    //const response = await axios.get('http://jsonplaceholder.typicode.com/posts');
-    //console.log(response)
-    const portfolios = await getPortfolios()
-    return {props: portfolios}
+  //const response = await axios.get('http://jsonplaceholder.typicode.com/posts');
+  //console.log(response)
+  const portfolios = await getPortfolios();
+  return { props: portfolios };
 }
