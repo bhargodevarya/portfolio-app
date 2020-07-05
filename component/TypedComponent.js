5edf2ab26e2feb55ef959b0ab5c244c746ab2721
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-05 14:45:59 +0530

    making typedcomponent take strings as props, passing from parent component

--- component/TypedComponent.js
+++ component/TypedComponent.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import Typed from "typed.js/lib/typed.min.js";
 
 export default class TypedComponent extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   componentDidMount() {
     console.log("ComponentDidMount");
 
     const options = {
-      strings: ["Developer", "Creator", "Curious"],
+      strings: this.props.strings.split(","),
       typeSpeed: 50,
       backSpeed: 50,
       loop: true,
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = (props) => {
         </Row>
       </Container>
       */}
-      <TypedComponent/>
+      <TypedComponent strings="Developer, Creator, Curious"/>
     </div>
   );
 };
