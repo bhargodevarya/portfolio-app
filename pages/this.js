efe796e549e39bf8b44d10f482442a1f03be1070
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-06 13:19:26 +0530

    Added this page

---
+++ pages/this.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ThisComp extends Component {
+    render() {
+        return (
+            <div style={{color: 'red', fontSize: 'large'}}>
+                This is the page to show this site's tech stack
+            </div>
+        )
+    }
+}
