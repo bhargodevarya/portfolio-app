9abfed1e947da9063717afb48040ec8da41ab65d
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-17 21:22:00 +0530

    Changed styling for the this page text

--- pages/this.js
+++ pages/this.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class ThisComp extends Component {
     render() {
         return (
-            <div style={{color: 'red', fontSize: 'large'}}>
+            <div style={{color: 'white', fontSize: 'large', fontWeight: 'Bold'}}>
                 Built with loads of love and react, nextjs, bootstrap, express, MongoDB and hosted on AWS
             </div>
         )
