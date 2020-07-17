a38f43b5e9874db26290fe572e8e25e2c9125a55
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-17 22:06:38 +0530

    Adding logos to the this page

--- pages/this.js
+++ pages/this.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ThisComp extends Component {
-    render() {
-        return (
-            <div style={{color: 'white', fontSize: 'large', fontWeight: 'Bold'}}>
-                Built with loads of love and react, nextjs, bootstrap, express, MongoDB and hosted on AWS
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{ color: "white", fontSize: "large", fontWeight: "Bold"}}>
+        Built with loads of love and the following
+        <div>
+          <img
+            style={{ width: "100px", paddingRight: "20px" }}
+            src="/next-js-seeklogo.com.svg"
+          ></img>
+          <img
+            style={{ width: "100px", paddingRight: "20px" }}
+            src="/react.svg"
+          ></img>
+          <img
+            style={{ width: "160px", paddingRight: "20px" }}
+            src="/mongodb.svg"
+          ></img>
+        </div>
+      </div>
+    );
+  }
 }
