15597e907322b2ce5364113795acc94f52e2b6a1
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-18 23:45:50 +0530

    Container for this page

--- pages/this.js
+++ pages/this.js
@@ -1,32 +1,40 @@
 import React, { Component } from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default class ThisComp extends Component {
   render() {
     return (
-      <div style={{ color: "white", fontSize: "large", fontWeight: "Bold"}}>
+      <div style={{ color: "white", fontSize: "large", fontWeight: "Bold", textAlign: 'center'}}>
         Built with loads of love and the following
-        <div>
-          <img
+        <Container style={{paddingTop:'10rem'}}>
+          <Row>
+            <Col>
+            <img
             style={{ width: "100px", paddingRight: "20px" }}
             src="/next-js-seeklogo.com.svg"
           ></img>
-          <img
+            </Col>
+            <Col>
+            <img
             style={{ width: "100px", paddingRight: "20px" }}
             src="/react.svg"
-          ></img>
-          <img
+          ></img></Col>
+          <Col><img
             style={{ width: "160px", paddingRight: "20px" }}
             src="/mongodb.svg"
-          ></img>
-          <img
+          ></img></Col>
+          <Col><img
             style={{ width: "100px", paddingRight: "20px" }}
             src="/aws.png"
-          ></img>
-          <img
+          ></img></Col>
+          <Col><img
             style={{ width: "140px", paddingRight: "20px" }}
             src="/nodejs.png"
-          ></img>
-        </div>
+          ></img></Col>
+          </Row>
+        </Container>
       </div>
     );
   }
