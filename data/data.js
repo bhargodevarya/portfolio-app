8eeaf4c2f35a229b7d00eeae632456dbd7fab8bb
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-25 13:54:31 +0530

    Updating portfolio data

--- data/data.js
+++ data/data.js
@@ -34,28 +34,28 @@ const MOVIE_DATA =
       {
         location: "Bengaluru, India",
         designation: "Software Associate",
-        organization: "Company1",
-        from: "Sep/2016",
-        to: "Sep/2018",
-        shortDesc: "This is some short desc",
+        organization: "Wipro",
+        from: "Feb/2011",
+        to: "Sep/2012",
+        shortDesc: "Wipro ",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed odio porttitor sem tincidunt scelerisque. Praesent ac tellus orci. Nullam sodales, nunc in finibus porttitor, justo leo scelerisque lectus, at volutpat purus libero sed nibh. In eleifend rhoncus feugiat. In rhoncus est a odio vestibulum auctor. Pellentesque nulla orci, placerat fringilla sodales sit amet, dignissim vel dui. Phasellus consequat ipsum id nulla fermentum dignissim vitae eu odio. Aenean nec dui euismod ipsum faucibus faucibus sit amet vel lectus. Vestibulum cursus risus eget sapien facilisis, eu aliquet diam laoreet. Proin vitae accumsan tortor, in mollis magna. Curabitur hendrerit quam vitae posuere tincidunt. Sed erat enim, blandit a viverra eu, vehicula a mi"
+      },
+      {
+        location: "Bengaluru, India",
+        designation: "Software Associate",
+        organization: "Wipro",
+        from: "Oct/2012",
+        to: "Apr/2014",
+        shortDesc: "Wipro CUCM/UCCX",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed odio porttitor sem tincidunt scelerisque. Praesent ac tellus orci. Nullam sodales, nunc in finibus porttitor, justo leo scelerisque lectus, at volutpat purus libero sed nibh. In eleifend rhoncus feugiat. In rhoncus est a odio vestibulum auctor. Pellentesque nulla orci, placerat fringilla sodales sit amet, dignissim vel dui. Phasellus consequat ipsum id nulla fermentum dignissim vitae eu odio. Aenean nec dui euismod ipsum faucibus faucibus sit amet vel lectus. Vestibulum cursus risus eget sapien facilisis, eu aliquet diam laoreet. Proin vitae accumsan tortor, in mollis magna. Curabitur hendrerit quam vitae posuere tincidunt. Sed erat enim, blandit a viverra eu, vehicula a mi"
       },
       {
         location: "Bengaluru, India",
         designation: "Senior Software Associate",
-        organization: "Company1",
-        from: "Sep/2018",
-        to: "Jul/2020",
-        shortDesc: "This is some short desc",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed odio porttitor sem tincidunt scelerisque. Praesent ac tellus orci. Nullam sodales, nunc in finibus porttitor, justo leo scelerisque lectus, at volutpat purus libero sed nibh. In eleifend rhoncus feugiat. In rhoncus est a odio vestibulum auctor. Pellentesque nulla orci, placerat fringilla sodales sit amet, dignissim vel dui. Phasellus consequat ipsum id nulla fermentum dignissim vitae eu odio. Aenean nec dui euismod ipsum faucibus faucibus sit amet vel lectus. Vestibulum cursus risus eget sapien facilisis, eu aliquet diam laoreet. Proin vitae accumsan tortor, in mollis magna. Curabitur hendrerit quam vitae posuere tincidunt. Sed erat enim, blandit a viverra eu, vehicula a mi"
-      },
-      {
-        location: "Bengaluru, India",
-        designation: "Senior Software Associate",
-        organization: "Company1",
-        from: "Sep/2018",
-        to: "Jul/2020",
-        shortDesc: "This is some short desc",
+        organization: "CTS",
+        from: "May/2014",
+        to: "Sep/2016",
+        shortDesc: "CTS SUVA team lead",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed odio porttitor sem tincidunt scelerisque. Praesent ac tellus orci. Nullam sodales, nunc in finibus porttitor, justo leo scelerisque lectus, at volutpat purus libero sed nibh. In eleifend rhoncus feugiat. In rhoncus est a odio vestibulum auctor. Pellentesque nulla orci, placerat fringilla sodales sit amet, dignissim vel dui. Phasellus consequat ipsum id nulla fermentum dignissim vitae eu odio. Aenean nec dui euismod ipsum faucibus faucibus sit amet vel lectus. Vestibulum cursus risus eget sapien facilisis, eu aliquet diam laoreet. Proin vitae accumsan tortor, in mollis magna. Curabitur hendrerit quam vitae posuere tincidunt. Sed erat enim, blandit a viverra eu, vehicula a mi"
       }
     ]
