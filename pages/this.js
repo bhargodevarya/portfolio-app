67895e9a489ddf13a8b0343530a31e421f1f8473
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-25 17:49:17 +0530

    Rendering tech logo from a function

--- pages/this.js
+++ pages/this.js
@@ -3,46 +3,42 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import { getTechnologies } from '../data/data'
+
 export default class ThisComp extends Component {
+  constructor(props) {
+    super(props)
+    this.techComp = this.techComp.bind(this)
+  }
+
+  techComp(technologies) {
+    return technologies.map((tech) => (
+      <Col>
+        <a href={tech.url}>
+          <img
+            style={tech.style}
+            src={tech.image}
+          ></img>
+        </a>
+      </Col>
+    ));
+  }
+
   render() {
     return (
       <div className="thisText">
         Built with loads of love and the following
         <Container fluid="sm">
           <Row xs={2} md={3}>
-            <Col>
-              <a href='http://www.nextjs.com'><img
-                style={{ width: "100px", paddingRight: "20px" }}
-                src="/next-js-seeklogo.com.svg"
-              ></img></a>
-            </Col>
-            <Col>
-              <a href="http://www.reactjs.org"><img
-                style={{ width: "100px", paddingRight: "20px" }}
-                src="/react.svg"
-              ></img></a>
-            </Col>
-            <Col>
-              <a href="https://www.mongodb.com/"><img
-                style={{ width: "160px", paddingRight: "20px" }}
-                src="/mongodb.svg"
-              ></img></a>
-            </Col>
-            <Col>
-              <a href="https://aws.amazon.com/"><img
-                style={{ width: "100px", paddingRight: "20px" }}
-                src="/aws.png"
-              ></img></a>
-            </Col>
-            <Col>
-              <a href="https://nodejs.org/en/"><img
-                style={{ width: "140px", paddingRight: "20px" }}
-                src="/nodejs.png"
-              ></img></a>
-            </Col>
+            {this.techComp(this.props.technologies)}
           </Row>
         </Container>
       </div>
     );
   }
+}
+
+export async function getStaticProps() {
+  const technologies = await getTechnologies()
+  return {props: technologies}
 }
