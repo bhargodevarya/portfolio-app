c495dae9da46cdffbf2e9aaf7543ce683da4c8cd
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-25 18:04:14 +0530

    Landing page cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import {getMovies} from "../data/data";
+import { getMovies } from "../data/data";
 
-import MyNavBar from '../component/RBNavBar'
-
-import TypedComponent from '../component/TypedComponent'
+import TypedComponent from "../component/TypedComponent";
 
 const Home = (props) => {
   return (
-    <div style={{paddingTop: '7rem'}}>
-      {/*
-      <Container>
-        <Row>This is the first row</Row>
-        <Row>
-          <MyCaraousel movies={props.movies} />
-        </Row>
-      </Container>
-      */}
-      <TypedComponent strings="Backend, Frontend, And everything in between"/>
-      {/* <img className="imageHome" src="/animation_4.gif"></img> */}
+    <div style={{ paddingTop: "7rem" }}>
+      <TypedComponent strings="Backend, Frontend, And everything in between" />
       <Container>
         <Row>
-        <p className="typedIntro">
-        Welcome to my site.
-        I hope this makes you curious.
-        Feel free to explore and you can contact me here</p>
+          <p className="typedIntro">{props.introText}</p>
         </Row>
       </Container>
     </div>
@@ -37,11 +23,15 @@ const Home = (props) => {
  */
 Home.getInitialProps = async () => {
   console.log("hello from home");
-  //This is normally a newtowk call or something similar that usually returns a Promise.
+  //This is normally a newtork call or something similar that usually returns a Promise.
   const movies = await getMovies();
   movies.map((m) => console.log("From home ", m.name));
   //return the props directly, what ever is returned is available on props variable in the component
-  return { movies };
+  return {
+    movies,
+    introText:
+      "Welcome to my site. I hope this makes you curious. Feel free to explore and you can contact me here",
+  };
 };
 
 export default Home;
