cda98b15ab84a8c2058e56c79ace38e4ab6edd32
Bhargodev Arya <bhargodevarya@gmail.com>
2020-09-03 22:14:58 +0530

    Deleting some files and refactoring for deployment

--- component/RBNavBar.js
+++ component/RBNavBar.js
@@ -12,7 +12,6 @@ const RBNavBar = () => {
           <Nav.Link href="/" className="myNavItem">Home</Nav.Link>
           <Nav.Link href="contact" className= "myNavItem">Contact</Nav.Link>
           <Nav.Link href="about" className= "myNavItem">About</Nav.Link>
-          <Nav.Link href="book" className= "myNavItem">Books</Nav.Link>
           <Nav.Link href="portfolio" className= "myNavItem">Portfolio</Nav.Link>
           <Nav.Link href="this" className= "myNavItem">This</Nav.Link>
         </Nav>
