ffc7fe42836a989864d3e24e9ff0a77407fdd5ed
Bhargodev Arya <bhargodevarya@gmail.com>
2020-10-02 22:04:36 +0530

    Some cleanup and adding portfolio json

--- pages/this.js
+++ pages/this.js
@@ -4,8 +4,6 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import { getTechnologies } from '../data/data'
-
 export default class ThisComp extends Component {
   constructor(props) {
     super(props)
